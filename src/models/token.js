b750a87063f6f3d86450939bf25024ae05c3f1c7
Betul <betulkoru06@gmail.com>
2024-04-15 20:36:42 +0300

    token model built

--- src/models/token.js
+++ src/models/token.js
@@ -1,6 +1,26 @@
-"use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+'use strict'
+
+const { mongoose } = require('../configs/dbConnection');
+
+const TokenSchema = new mongoose.Schema({
+    
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        index: true,
+    }, 
+
+    token: {
+        type: String,
+        trim: true,
+        required: true,
+        index: true,
+    } 
+
+} , {
+    collection: 'tokens',
+    timestamps: true
+});
+
+module.exports = mongoose.model('Token', TokenSchema);
