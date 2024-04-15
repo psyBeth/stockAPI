2832f4f67c77b9623f9952de8174c6cca7396e93
Betul <betulkoru06@gmail.com>
2024-04-15 20:20:08 +0300

    user model intro

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,91 @@
-"use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+'use strict'
+
+const { mongoose } = require('../configs/dbConnection');
+
+/*   
+    EXAMPLE 
+{
+    "username": "admin",
+    "password": "aA*123456",
+    "email": "admin@site.com",
+    "firstName": "admin",
+    "lastName": "admin",
+    "isActive": true,
+    "isStaff": true,
+    "isAdmin": true
+}
+{
+    "username": "staff",
+    "password": "aA*123456",
+    "email": "staff@site.com",
+    "firstName": "staff",
+    "lastName": "staff",
+    "isActive": true,
+    "isStaff": true,
+    "isAdmin": false
+}
+{
+    "username": "test",
+    "password": "aA*123456",
+    "email": "test@site.com",
+    "firstName": "test",
+    "lastName": "test",
+    "isActive": true,
+    "isStaff": false,
+    "isAdmin": false
+}
+*/
+
+const UserSchema = new mongoose.Schema({
+    // _id
+
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true
+    },
+
+    password: {
+        type: String,
+        trim: true,
+        require: true
+    },
+
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true        
+    },
+
+    firstName: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    isActive: {
+
+    },
+
+    isStaff: {
+
+    },
+
+    isAdmin:{
+
+    }
+
+} , {
+    collection: 'users',
+    timestamps: true
+});
