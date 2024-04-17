a6da61c420f67be3c5ea783a221f1855f88815a7
Betul <betulkoru06@gmail.com>
2024-04-17 21:20:54 +0300

    brand model

--- src/models/brand.js
+++ src/models/brand.js
@@ -1,6 +1,25 @@
-"use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
+'use strict'
+
 const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+
+const BrandSchema = new mongoose.Schema({
+    //_id
+
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+
+    image: {
+        type: String,
+        trim: true
+    }
+
+} , {
+    collection: 'brands',
+    timestamps: true
+});
+
+module.exports = mongoose.model('Brand', BrandSchema);
