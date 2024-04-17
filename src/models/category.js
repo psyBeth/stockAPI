8407a743e4658d1d2ccbdeb7f6f6873c78d29f39
Betul <betulkoru06@gmail.com>
2024-04-17 20:30:57 +0300

    category model

--- src/models/category.js
+++ src/models/category.js
@@ -1,6 +1,20 @@
-"use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+'use strict'
+
+const { mongoose } = require('../configs/dbConnection');
+
+const CategorySchema = new mongoose.Schema({
+    //_id
+    
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    }
+
+} , {
+    collection: 'categories',
+    timestamps: true
+});
+
+module.exports = mongoose.model('Category', CategorySchema);
