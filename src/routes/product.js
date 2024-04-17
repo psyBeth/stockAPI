c345c74d16cf4691a9f0aab813ca31887058449b
Betul <betulkoru06@gmail.com>
2024-04-17 21:39:49 +0300

    product router

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,3 +2,16 @@
 
 const router = require('express').Router();
 
+const product = require('../controllers/product');
+const permissions = require('../middlewares/permissions');
+
+// URL: /products:
+
+router.route('/(:id)?')
+    .post(permissions.isAdmin, product.create)
+    .get(permissions.isStaff, product.read)
+    .put(permissions.isAdmin, product.update)
+    .patch(permissions.isAdmin, product.update)
+    .delete(permissions.isAdmin, product.delete)
+
+module.exports = router;
