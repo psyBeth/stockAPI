b496f40dc90d78f8ac42cdb4830b20adf18ca771
Betul <betulkoru06@gmail.com>
2024-04-17 21:25:18 +0300

    brand controller and router

--- src/routes/brand.js
+++ src/routes/brand.js
@@ -1,6 +1,15 @@
-"use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
-const router = require('express').Router()
-/* ------------------------------------------------------- */
+'use strict'
+
+const router = require('express').Router();
+
+const brand = require('../controllers/brand');
+const permissions = require('../middlewares/permissions');
+
+router.route('/(:id)?')
+    .post(permissions.isAdmin, brand.create)
+    .get(permissions.isStaff, brand.read)
+    .put(permissions.isAdmin, brand.update)
+    .patch(permissions.isAdmin, brand.update)
+    .delete(permissions.isAdmin, brand.delete)
+
+module.exports = router;
