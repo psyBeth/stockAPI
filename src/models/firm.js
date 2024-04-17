8e88806ad4fbbccb6b79eaadc1e118eee4cc6626
Betul <betulkoru06@gmail.com>
2024-04-17 21:29:10 +0300

    firm model

--- src/models/firm.js
+++ src/models/firm.js
@@ -1,6 +1,26 @@
-"use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+'use strict'
+
+const { mongoose } = require('../configs/dbConnection');
+
+const FirmSchema = new mongoose.Schema({
+    //_id
+
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+
+    phone: String,
+
+    adress: String,
+
+    image: String
+
+} , {
+    collection: 'firms',
+    timestamps: true
+});
+
+module.exports = mongoose.model('Firm', FirmSchema);
